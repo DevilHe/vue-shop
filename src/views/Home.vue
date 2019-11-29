edf123572542e93dc391d8230e40edab5a11a939
wanglh <wanglh@jowto.com>
2019-11-29 22:25:13 +0800

    今日推荐vue-awesome-swiper轮播图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,12 +18,32 @@
         </van-swipe-item>
       </van-swipe>
     </div>
+
+    <!-- 今日推荐 -->
+    <div class="hot">
+      <p class="hot-title">今日推荐</p>
+      <swiper class="hot-swiper" :options="swiperOption">
+        <swiper-slide v-for="(item, index) in hotProducts" :key="index">
+          <div class="hot-swiper-content">
+            <img :src="item.img" alt />
+            <div>{{ item.name }}</div>
+            <div>￥{{ item.price }}</div>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script>
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
   name: "home",
+  components: {
+    swiper,
+    swiperSlide
+  },
   data() {
     return {
       carouselItem: [
@@ -35,27 +55,106 @@ export default {
         {
           name: "img2",
           imgSrc: require("../assets/images/img2.jpg")
-          // imgSrc: "https://img.yzcdn.cn/vant/apple-2.jpg"
         },
         {
           name: "img3",
           imgSrc: require("../assets/images/img3.jpg")
-          // imgSrc: "https://img.yzcdn.cn/vant/apple-3.jpg"
         }
-      ]
+      ],
+      hotProducts: [
+        {
+          name: "花牛苹果",
+          img: require("../assets/images/img-apple.png"),
+          price: "29.90"
+        },
+        {
+          name: "琯溪蜜柚",
+          img: require("../assets/images/img-pomelo.png"),
+          price: "58.80"
+        },
+        {
+          name: "车厘子",
+          img: require("../assets/images/img-cherry.png"),
+          price: "129.00"
+        },
+        {
+          name: "柑橘",
+          img: require("../assets/images/img-citrus.png"),
+          price: "59.90"
+        },
+        {
+          name: "草莓",
+          img: require("../assets/images/img-strawberry.png"),
+          price: "76.80"
+        }
+      ],
+      swiperOption: {
+        slidesPerView: 3
+      }
     };
   }
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+.container {
+  background-color: #eee;
+}
+.nav-title {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999 !important;
+}
 .carousel {
+  margin-top: 1rem;
   height: 3rem;
   &-item {
     img {
-      width: 100rem;
+      width: 100%;
       height: 3rem;
     }
+  }
+}
+.hot {
+  margin-top: 0.2rem;
+  background-color: #fff;
+  &-title {
+    width: 100%;
+    height: 0.5rem;
+    padding-left: 0.2rem;
+    line-height: 0.5rem;
+    box-sizing: border-box;
+  }
+  &-swiper {
+    &-content {
+      width: 2rem;
+      text-align: center;
+      img {
+        width: 2rem;
+        height: 2rem;
+      }
+    }
+  }
+}
+// 热门商品
+.variety {
+  margin-top: 0.2rem;
+  background-color: #fff;
+  text-align: center;
+  margin-bottom: 1rem;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  &-item {
+    flex-basis: 45%;
+  }
+  img {
+    width: 2rem;
+    height: 2rem;
   }
 }
 </style>
